2f647757ab10773ecffc7a65c4f396ecac3fe74f
Pooja Singh <pooja@wonderpillars.in>
2018-07-28 00:59:38 +0530

    Project Initialize

---
+++ ios/Delgate-Bridging-Header.h
@@ -0,0 +1,21 @@
+//
+//  Delgate-Bridging-Header.h
+//  DelGate
+//
+//  Created by Ankush on 4/13/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#ifndef Delgate_Bridging_Header_h
+#define Delgate_Bridging_Header_h
+
+#import "React/RCTBridge.h"
+#import "React/RCTViewManager.h"
+#import "React/RCTUIManager.h"
+#import "React/UIView+React.h"
+#import "React/RCTBridgeModule.h"
+#import "React/RCTEventDispatcher.h"
+#import "React/RCTEventEmitter.h"
+#import "React/RCTFont.h"
+
+#endif /* Delgate_Bridging_Header_h */
---
+++ ios/test.swift
@@ -0,0 +1,9 @@
+//
+//  test.swift
+//  DelGate
+//
+//  Created by Ankush on 4/13/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+import Foundation
